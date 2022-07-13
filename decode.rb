d70951aaecf72117832826df8a34212a40d27c68
EidHachem <eidhachem1@gmail.com>
2022-07-13 15:35:09 +0300

    Add decode_msg method and linters files

--- decode.rb
+++ decode.rb
@@ -1,50 +1,59 @@
-def decode_char(char)
-  morse_dict = {
-    "A" => ".-",
-    "B" => "-...",
-    "C" => "-.-.",
-    "D" => "-..",
-    "E" => ".",
-    "F" => "..-.",
-    "G" => "--.",
-    "H" => "....",
-    "I" => "..",
-    "J" => ".---",
-    "K" => "-.-",
-    "L" => ".-..",
-    "M" => "--",
-    "N" => "-.",
-    "O" => "---",
-    "P" => ".--.",
-    "Q" => "--.-",
-    "R" => ".-.",
-    "S" => "...",
-    "T" => "-",
-    "U" => "..-",
-    "V" => "...-",
-    "W" => ".--",
-    "X" => "-..-",
-    "Y" => "-.--",
-    "Z" => "--..",
-    " " => " ",
-    "1" => ".----",
-    "2" => "..---",
-    "3" => "...--",
-    "4" => "....-",
-    "5" => ".....",
-    "6" => "-....",
-    "7" => "--...",
-    "8" => "---..",
-    "9" => "----.",
-    "0" => "-----"
-  }
+$morse_dict = {
+  'A' => '.-',
+  'B' => '-...',
+  'C' => '-.-.',
+  'D' => '-..',
+  'E' => '.',
+  'F' => '..-.',
+  'G' => '--.',
+  'H' => '....',
+  'I' => '..',
+  'J' => '.---',
+  'K' => '-.-',
+  'L' => '.-..',
+  'M' => '--',
+  'N' => '-.',
+  'O' => '---',
+  'P' => '.--.',
+  'Q' => '--.-',
+  'R' => '.-.',
+  'S' => '...',
+  'T' => '-',
+  'U' => '..-',
+  'V' => '...-',
+  'W' => '.--',
+  'X' => '-..-',
+  'Y' => '-.--',
+  'Z' => '--..',
+  ' ' => ' ',
+  '1' => '.----',
+  '2' => '..---',
+  '3' => '...--',
+  '4' => '....-',
+  '5' => '.....',
+  '6' => '-....',
+  '7' => '--...',
+  '8' => '---..',
+  '9' => '----.',
+  '0' => '-----'
+}
 
-  return morse_dict.key(char)
+def decode_char(char)
+  morse_dict.key(char)
 end
 
 def decode_word(word)
-  array = word.split(' ')
+  array = word.split
   english = []
-  array.each {|letter| english.insert(english.length, decode_char(letter))}
-  return english.join('')
+  array.each { |letter| english.insert(english.length, decode_char(letter)) }
+  english.join
 end
+
+def decode_msg(msg)
+  array = msg.split('   ')
+  english = []
+  array.each { |word| english.insert(english.length, decode_word(word)) }
+  english.join(' ')
+end
+
+print decode_msg('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
